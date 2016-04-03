d9d0265fd7887e5b2f6d75c2e817e66a8e8bf2eb
Jhonatan Oliveira <jhonatanoliveira@gmail.com>
2016-04-02 22:31:11 -0600

    The comparison paper, with the code for implementing SPNs with GPU and CPU.

---
+++ the_comparison/README.md
@@ -0,0 +1,5 @@
+# The Probelm
+
+## About
+
+Sum-product networks (SPNs) are a layer-wise mathematical models for learning and inference with probabilistic graphical model. The main advantage of SPNs is that a learned model, under certain conditions, is guaranteed to have tractable inference in polynomial time, besides of inference being exact. Given a SPN, inference is done by propagating up on the network. In this paper, it is shown how the propagation can be done in parallel.
--- the_comparison/material/test.py
+++ the_comparison/material/test.py
@@ -6,32 +6,25 @@ import tensorflow as tf
 
 f = open("results.csv", "w")
 
+for i in range(1,50):
 
-INPUT_SIZE = 10
-OUTPUT_SIZE = INPUT_SIZE
-nb_class = 3
+    INPUT_SIZE = 10 * i
+    OUTPUT_SIZE = INPUT_SIZE
+    nb_class = 3
 
-# global parameters
-batch_size = 128
-nb_epoch = 40
+    batch_size = 128
+    nb_epoch = 40
 
-np.random.seed(123)
+    np.random.seed(123)
 
-X_train = np.random.rand(INPUT_SIZE, nb_class)
-Y_train = np.random.rand(OUTPUT_SIZE, nb_class)
+    X_train = np.random.rand(INPUT_SIZE, nb_class)
+    Y_train = np.random.rand(OUTPUT_SIZE, nb_class)
 
-X_test = np.random.rand(INPUT_SIZE)
-Y_test = np.random.rand(OUTPUT_SIZE)
-
-for i in range(1,51):
-
-    print("--> Running at %d <--" % i)
-
-    print("--> Compiling model...")
+    X_test = np.random.rand(INPUT_SIZE)
+    Y_test = np.random.rand(OUTPUT_SIZE)
 
     start_time = time.time()
 
-    print("--> Building model...")
     model = Sequential()
     model.add(Dense(INPUT_SIZE, input_shape=(nb_class,)))
     model.add(Activation('linear'))
@@ -41,8 +34,6 @@ for i in range(1,51):
 
     final_time = time.time()
     diff_time = final_time - start_time
-    print("--> Exec time:")
-    print(diff_time)
 
     f.write(str(i)+","+str(diff_time)+","+"\n")
 
